92d0b6ce58cb0af80ceecb8bc0f1e5d52e13bcde
ArkhamCookie <github@mail.theuntitledproject.dev>
2023-05-30 00:44:29 -0500

    working on changing how styling works

--- src/bin/discord-webhook.js
+++ src/bin/discord-webhook.js
@@ -1,8 +1,6 @@
 #!/usr/bin/env node
 
 // import { program } from 'commander'
-import chalk from 'chalk'
 import { send } from '../lib/send.js'
 
-send('Hello **cmd**')
-console.log(chalk.green('Hello ') + chalk.green.bold('cmd'))
+send('foo')
--- src/lib/styling.js
+++ src/lib/styling.js
@@ -1,34 +1,44 @@
 import chalk from 'chalk'
-import { send } from '../lib/send.js'
-
-let message
-
-/*
-const italic = '*' + text + '*'
-const bold = '**' + text + '**'
-const boldItalic = '***' + text + '***'
-const strikethrough = '~~' + text + '~~'
-*/
+// import { send } from '../lib/send.js'
 
 function discordStyle(text, styling) {
 	if (styling === 'italic') {
-		message = '*' + text + '*'
-		send(message)
-		console.log(chalk.bold(text))
+		// console.log(chalk.bold(text))
+		const italicText = '*' + text + '*'
+		return italicText
 	}
 
 	if (styling === 'bold') {
-		message = '**' + text + '**'
-		send(message)
-		console.log(chalk.bold(text))
+		// console.log(chalk.bold(text))
+		const boldText = '**' + text + '**'
+		return boldText
 	}
 
 	if (styling === 'strikethrough') {
-		message = '~~' + text + '~~'
-		send(message)
-		console.log(chalk.yellow(text))
+		// console.log(chalk.yellow(text))
+		const strikethroughText = '~~' + text + '~~'
+		return strikethroughText
 	}
-	// message = italic(text)
 }
 
-export { discordStyle }
+function linkStyle(link, type, embedded) {
+	if (type === undefined) {
+		type = 'url'
+	} else
+	if (type === 'url') {
+		console.log(type)
+	}
+
+	if (embedded === undefined) {
+		embedded = true
+	} else
+	if (embedded === false) {
+		link = '<' + link + '>'
+		return link
+	}
+	console.log(chalk.blue(link))
+}
+
+linkStyle('https://example.com')
+
+export { discordStyle, linkStyle }
