eab723afd904012e92d2030c306eac2aeed90b80
ArkhamCookie <git@mail.theuntitledproject.dev>
2023-06-03 22:41:14 -0500

    now using deno env

--- src/lib/send.js
+++ src/lib/send.js
@@ -1,8 +1,8 @@
-import { Webhook } from 'minimal-discord-webhook-node'
-import 'dotenv/config'
+import { Webhook } from 'npm:minimal-discord-webhook-node@1'
+import 'https://deno.land/std@0.190.0/dotenv/load.ts'
 
 const hook = new Webhook({
-	url: process.env.WEBHOOK_URL,
+	url: Deno.env.get('WEBHOOK_URL'),
 	throwErrors: true,
 	retryOnLimit: false
 })
