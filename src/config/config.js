c030fd41fdb4130ae33ac8f44b8e8f98c2d8d3ed
ArkhamCookie <git@mail.theuntitledproject.dev>
2023-06-04 01:45:07 -0500

    reworked lib/send to make better use of config.js

--- src/config/config.js
+++ src/config/config.js
@@ -1,10 +1,13 @@
 import { Webhook } from 'npm:minimal-discord-webhook-node@1'
 import 'https://deno.land/std@0.190.0/dotenv/load.ts'
 
+const TOKEN = Deno.env.get('TOKEN')
+const CLIENT_ID = Deno.env.get('CLIENT_ID')
+
 const hook = new Webhook({
-	url: process.env.WEBHOOK_URL,
+	url: Deno.env.get('WEBHOOK_URL'),
 	throwErrors: true,
 	retryOnLimit: false
 })
 
-export { hook }
+export { hook, TOKEN, CLIENT_ID }
--- src/lib/send.js
+++ src/lib/send.js
@@ -1,14 +1,7 @@
-import { Webhook } from 'npm:minimal-discord-webhook-node@1'
-import 'https://deno.land/std@0.190.0/dotenv/load.ts'
-
-const hook = new Webhook({
-	url: Deno.env.get('WEBHOOK_URL'),
-	throwErrors: true,
-	retryOnLimit: false
-})
+import { hook } from '../config/config.js'
 
 function send(message) {
 	hook.send(message)
 }
 
-export { hook, send }
+export { send }
