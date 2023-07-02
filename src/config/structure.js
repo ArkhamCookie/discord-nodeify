c39d5f211c92f1722b9eddde999b555e01bf29f1
ArkhamCookie <git@mail.theuntitledproject.dev>
2023-07-01 20:13:18 -0500

    switching machines

---
+++ src/config/structure.js
@@ -0,0 +1,29 @@
+// import
+
+class Structure {
+	Youtube(videoUrl, title, description, thumbnail) {
+		this.link = videoUrl
+
+		this.id = videoID
+		// <media:group>
+		this.title = title // <media:title>
+		this.content = description // <media:description>
+		this.thumbnail = thumbnail // <media:thumbnail>
+	}
+
+	// <author>
+	author(author) {
+		this.author = author
+		this.uploadTime = author
+	}
+
+	// <entry>
+
+	// <media:group>
+	media(media) {
+		this.title = '' // <media:title>
+		this.content = '' // <media:description>
+		this.thumbnail = '' // <media:thumbnail>
+	}
+	// </entry>
+}
