a654d9f1611fd6f976ae757a0a32549e09c548b2
ArkhamCookie <git@mail.theuntitledproject.dev>
2023-07-19 02:34:57 -0500

    removed config testing planned in seperated file

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,3 @@
-import { existsSync } from 'https://deno.land/std@0.193.0/fs/mod.ts'
 import { Webhook } from 'discord-webhook'
 import 'dotenv'
 
@@ -13,42 +12,4 @@ const HOOK = new Webhook({
 const CLIENT_ID = Deno.env.get('CLIENT_ID')
 const BOT_TOKEN = Deno.env.get('BOT_TOKEN')
 
-/** Config Options */
-const SERVER_ID = Deno.env.get('SERVER_ID')
-
-/** Feed(s) Config */
-let CONFIG_FILE
-if (
-	existsSync(Deno.cwd() + '/config.json', {
-		isReadable: true,
-		isFile: true
-	})
-) {
-	CONFIG_FILE = Deno.cwd() + '/config.json'
-} else if (
-	existsSync(Deno.cwd() + '/.env', {
-		isReadable: true,
-		isFile: true
-	})
-) {
-	CONFIG_FILE = Deno.cwd() + '/.env'
-} else {
-	CONFIG_FILE = false
-}
-
-/** Config Verify */
-function emptyCheck(option) {
-	if (!option) {
-		console.warn(option + 'isn\'t set')
-		return false
-	}
-	return true
-}
-
-const options = [HOOK.hookURL, CLIENT_ID, BOT_TOKEN, SERVER_ID]
-
-for (let i = 0; i < options.length; i++) {
-	emptyCheck(options[i])
-}
-
-export { BOT_TOKEN, CLIENT_ID, CONFIG_FILE, HOOK, SERVER_ID }
+export { BOT_TOKEN, CLIENT_ID, HOOK }
