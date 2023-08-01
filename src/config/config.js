17b1c49ad2a656997f74ff44ee6db8035e899230
ArkhamCookie <git@mail.theuntitledproject.dev>
2023-08-01 03:09:57 -0500

    created easy way to fetch feed

--- src/config/config.js
+++ src/config/config.js
@@ -1,4 +1,5 @@
 import { Webhook } from 'discord-webhook'
+import { parseFeed } from 'rss'
 import 'dotenv'
 
 /** Webhook Config */
@@ -12,4 +13,20 @@ const HOOK = new Webhook({
 const CLIENT_ID = Deno.env.get('CLIENT_ID')
 const BOT_TOKEN = Deno.env.get('BOT_TOKEN')
 
-export { BOT_TOKEN, CLIENT_ID, HOOK }
+/** Feeds */
+// const youtubeBase = 'https://youtube.com/feeds/videos.xml?channel_id='
+
+async function fetchFeed(target) {
+	const response = await fetch(target)
+
+	if (!response.status === 200) {
+		console.error(response.status + ': ' + response.statusText)
+		return false
+	}
+
+	const xml = await response.text()
+	const feed = await parseFeed(xml)
+	return feed
+}
+
+export { BOT_TOKEN, CLIENT_ID, fetchFeed, HOOK }
