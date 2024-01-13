19b85ba15386313148ef7161e8d7ac5e5849c228
ArkhamCookie <github@mail.theuntitledproject.dev>
2024-01-13 12:14:33 -0600

    removed testing code from file

--- src/config/feeds/substack-podcast.js
+++ src/config/feeds/substack-podcast.js
@@ -1,20 +1,7 @@
-import { parseFeed } from 'rss'
 import { MessageBuilder } from 'discord-webhook'
 import { HOOK } from '../config.js'
 
-// https://api.substack.com/feed/podcast/1180773.rss
-const base = 'https://api.substack.com/feed/podcast/'
-const id = '1180773.rss'
-const target = base + id
-
-const response = await fetch(target)
-if (!response.status === 200) {
-	console.log(response.status)
-}
-const xml = await response.text()
-const feed = await parseFeed(xml)
-
-class PodcastFeed {
+class SubstackPodcastFeed {
 	constructor(feed) {
 		const { entries } = feed
 
@@ -44,8 +31,4 @@ class PodcastFeed {
 	}
 }
 
-// console.log(feed)
-// const podcastReport = new PodcastFeed(feed)
-// podcastReport.send()
-
-export { PodcastFeed }
+export { SubstackPodcastFeed }
